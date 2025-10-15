16fc9ca9f3e29c336ba2c0c04f7722160a3100e0
DeITdev <danarikram@gmail.com>
2025-10-16 05:23:14 +0700

    add head title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 
 import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from '../components';
@@ -8,6 +9,12 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }) => (
   <NFTProvider>
     <ThemeProvider attribute="class">
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <div className="dark:bg-nft-dark bg-white min-h-screen">
         <Navbar />
         <div className="pt-65">
